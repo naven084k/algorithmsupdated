e7ae6a11d20b3a159a6d8b2bfff437b2384727c5
Naveen <Naveen@KGML037>
2017-10-20 12:18:28 +0530

    program to print strings of length n with k-ary bit

---
+++ src/main/java/com/learn/geeks/backtracking/PrintNumbersWithNBit.java
@@ -0,0 +1,27 @@
+package com.learn.geeks.backtracking;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrintNumbersWithNBit {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter k to print k-ary numbers");
+		int k=scanner.nextInt();
+		k_ary(3,new int[3],k);
+
+	}
+	
+	static void k_ary(int n,int[] a,int k) {
+		if(n == 0)
+			System.out.println(Arrays.toString(a));
+		else {
+			for(int i=0;i<=k;i++) {
+				a[n-1] = i;
+				k_ary(n-1,a,k);
+			}
+		}
+	}
+
+}
