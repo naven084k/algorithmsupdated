f9a4db007975aa2aa1baa7e8002e235384c924d3
naven084k <naven084k@gmail.com>
2018-02-06 16:57:39 +0530

    largest rectangle....

---
+++ src/main/java/com/learn/geeks/stack/LargestRectangle.java
@@ -0,0 +1,25 @@
+package com.learn.geeks.stack;
+
+import java.util.Stack;
+
+public class LargestRectangle {
+	public static void main(String[] args) {
+		largestRectangleArea(new int[] {1,2,3,1,2});
+	}
+    public static int largestRectangleArea(int[] height) {
+        int len = height.length;
+        Stack<Integer> s = new Stack<Integer>();
+        int maxArea = 0;
+        for(int i = 0; i <= len; i++){
+            int h = (i == len ? 0 : height[i]);
+            if(s.isEmpty() || h >= height[s.peek()]){
+                s.push(i);
+            }else{
+                int tp = s.pop();
+                maxArea = Math.max(maxArea, height[tp] * (s.isEmpty() ? i : i - 1 - s.peek()));
+                i--;
+            }
+        }
+        return maxArea;
+    }
+}
